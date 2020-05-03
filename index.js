30ce1568db814cc0471b650eed20a67a56f2fa03
m98jk <al7joker111@gmail.com>
2020-05-03 14:01:38 +0300

    adding more methods

--- index.js
+++ index.js
@@ -5,8 +5,12 @@ const path = require('path');
 
 app.use((res, req, next) => {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Methods', 'GET');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json'
+  );
   next();
 });
 
